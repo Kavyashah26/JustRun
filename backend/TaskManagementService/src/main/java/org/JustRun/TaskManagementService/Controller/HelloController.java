a35a7b2c3683743e58538787d495308d9b331f75
Kavya Shah <kavyaatulshahpatan@gmail.com>
2025-05-06 12:06:14 +0530

    Init for Task Management Service

---
+++ backend/TaskManagementService/src/main/java/org/JustRun/TaskManagementService/Controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.JustRun.TaskManagementService.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Task Management Service is working!";
+    }
+}
